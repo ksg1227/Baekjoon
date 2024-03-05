7d0816fa30240f26f0fbb45dcd83bd1d38a1f407
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-03-05 17:48:50 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 516 ms, Memory: 50324 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -21,30 +21,49 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<Integer, String> map2 = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
 
-        for(int i=1;i<=N;i++){
+        for(int i=0;i<N;i++){
             String pocketMonName = br.readLine();
-            map1.put(pocketMonName, i);
-            map2.put(i, pocketMonName);
+            String pocketMonNum = Integer.toString(i+1);
+
+            map.put(pocketMonName, pocketMonNum);
+            map.put(pocketMonNum, pocketMonName);
         }
 
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0;i<M;i++){
+            String str = br.readLine();
 
-            String str = "";
-
-            try{
-                str = br.readLine();
-
-                int num = Integer.parseInt(str);
-
-                System.out.println(map2.get(num));
-            } catch (NumberFormatException e) {
-                System.out.println(map1.get(str));
-            }
-            
+            sb.append(map.get(str)).append("\n");
         }
+
+        System.out.println(sb);
+
+//        HashMap<String, Integer> map1 = new HashMap<>();
+//        HashMap<Integer, String> map2 = new HashMap<>();
+//
+//        for(int i=1;i<=N;i++){
+//            String pocketMonName = br.readLine();
+//            map1.put(pocketMonName, i);
+//            map2.put(i, pocketMonName);
+//        }
+//
+//        for(int i=0;i<M;i++){
+//
+//            String str = "";
+//
+//            try{
+//                str = br.readLine();
+//                int num = Integer.parseInt(str);
+//
+//                System.out.println(map2.get(num));
+//            } catch (NumberFormatException e) {
+//                System.out.println(map1.get(str));
+//            }
+//
+//        }
 
     }
 
