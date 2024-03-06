4a44caf68223e2db86d5527459893265fa78f2c6
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-03-07 00:54:00 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    단계별로 풀어보기
+    심화 1단계
+    25083번 문제: 새싹
+*/
+
+public class Main {
+
+    static void solution() throws IOException {
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+
+    }
+
+    public static void main(String args[]) throws Exception {
+        new Main().solution();
+    }
+}
+
+
+
+
+
