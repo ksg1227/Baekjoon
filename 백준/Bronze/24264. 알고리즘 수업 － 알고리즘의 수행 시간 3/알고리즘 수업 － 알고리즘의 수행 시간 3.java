5eb138c767ca252306555a3be21660c3e186e0e8
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-03-07 00:53:30 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/*
+    단계별로 풀어보기
+    시간 복잡도
+    24263번 문제: 알고리즘 수업 - 알고리즘의 수행 시간1
+*/
+
+public class Main {
+
+    static void solution() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+
+    public static void main(String args[]) throws Exception {
+        new Main().solution();
+    }
+}
+
