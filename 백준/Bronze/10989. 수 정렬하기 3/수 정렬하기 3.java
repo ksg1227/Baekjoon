9beb8f24ceb1e69f7ddf5030c7b43a79548399dd
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-03-29 15:30:34 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1668 ms, Memory: 340136 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,36 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
-/*  
-    단계별로 풀어보기
-    정렬
-    2751번 문제: 수 정렬하기2
-*/
+/*
+    solved.ac
+    class2
+    10989번 문제 : 수 정렬하기 3
+ */
 
 public class Main {
 
     static void solution() throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[10001];
 
-        for(int i=0;i<10001;i++){
-            arr[i] = 0;
-        }
-
         for(int i=0;i<N;i++){
-            int index = Integer.parseInt(br.readLine());
-            arr[index]++;
+            int num = Integer.parseInt(br.readLine());
+            arr[num] += 1;
         }
 
-        StringBuilder sb = new StringBuilder();
-
         for(int i=0;i<10001;i++){
-            if(arr[i] != 0){
+            if(arr[i] > 0){
                 for(int j=0;j<arr[i];j++){
                     sb.append(i).append("\n");
                 }
@@ -39,9 +36,11 @@ public class Main {
 
         System.out.println(sb);
 
+
     }
-    public static void main(String args[]) throws Exception {
-        new Main().solution();
+
+
+    public static void main(String[] args) throws Exception {
+        Main.solution();
     }
 }
-
