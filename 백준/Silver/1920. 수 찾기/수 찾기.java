b7eeffe8045cfec2e35d1ca8354f2c39d3e00b0d
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-08-29 03:42:04 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 49956 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,50 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 /*
-    solved.ac
-    class2
-    1874번 문제 : 스택 수열
+    DoIt 코딩테스트
+    5장 탐색 - 이진 탐색
+    1920번 문제: 원하는 정수 찾기
  */
 
 public class Main {
 
+    static int N;
+    static int M;
+
+    static Set<Integer> set = new HashSet<Integer>();
+
     static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            int num = Integer.parseInt(st.nextToken());
+
+            set.add(num);
         }
 
-        int M = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(set.contains(num)){
+
+            if(set.contains(num)) {
                 sb.append(1).append("\n");
-            }else{
+            } else {
                 sb.append(0).append("\n");
             }
         }
 
         System.out.println(sb);
 
-
     }
 
     public static void main(String[] args) throws Exception {
         Main.solution();
     }
+
+
 }
