f9d889d0ae540d9a475f15d770368335bcaf1398
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2024-12-30 00:21:56 +0900

    [Silver II] Title: 폴짝폴짝, Time: 148 ms, Memory: 16212 KB -BaekjoonHub

---
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 스터디 1주차
+ * bfs / dfs
+ * 백준 1326번 : 폴짝폴짝
+ */
+
+public class Main {
+    // 최소 몇 번 점프가 필요한지를 체크하니 BFS로 탐색 가능.
+    // 1을 만난 시점에서는 한 방에 도착 가능
+
+    static int N;
+    static int[] num;
+    static int a;
+    static int b;
+
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        num = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(a));
+
+    }
+
+    static int bfs(int start) {
+        // 즉 왔던 위치로 다시 되돌아가는 경우는 배제해야함!!!!!
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0, start});
+
+        while (!queue.isEmpty()) {
+            int[] data = queue.poll();
+            int currentIndex = data[0];
+            int count = data[1];
+            int beforeIndex = data[2];
+            int currentNum = num[currentIndex];
+
+            if (Math.abs((b - currentIndex)) % currentNum == 0) {
+                return count + 1;
+            } else {
+                for (int i = currentIndex - currentNum; i >= 1; i -= currentNum) {
+                    if (beforeIndex != currentIndex || count == 0)
+                        queue.add(new int[]{i, count + 1, currentIndex});
+                }
+
+                for (int i = currentIndex + currentNum; i <= N; i += currentNum) {
+                    if (beforeIndex != currentIndex || count == 0)
+                        queue.add(new int[]{i, count + 1, currentIndex});
+                }
+            }
+
+        }
+
+        return-1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+}
+
+
+
