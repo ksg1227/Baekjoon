c1349f60cd14334620148daf9eda31bc1033e882
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2025-01-10 13:06:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,92 +3,82 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/*
-    단계별로 풀어보기
-    그래프와 순회
-    2667번 문제: 단지 번호 붙이기
-*/
+/**
+ * 스터디 1주차
+ * bfs / dfs
+ * 백준 2667번 : 단지 번호 붙이기
+ */
 
 public class Main {
 
-    static int[][] map;
-
-    static boolean[][] visit;
-
-    static ArrayList<Integer> result = new ArrayList<>();
-
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
-
     static int N;
-
-    static int count = 1;
-
+    static int[][] map;
+    static boolean[][] visited;
+    static List<Integer> list = new ArrayList<>();
+    static int count = 0;
+    static int result = 0;
 
     static void solution() throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
-        visit = new boolean[N][N];
+        visited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
-
             for (int j = 0; j < N; j++) {
                 map[i][j] = line.charAt(j) - '0';
             }
         }
 
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visit[i][j]) {
+                if(map[i][j] == 1 && !visited[i][j]) {
                     dfs(i, j);
-                    result.add(count);
-                    count = 1;
+                    count++;
+                    list.add(result);
+                    result = 0;
                 }
             }
         }
 
-        Collections.sort(result);
+        Collections.sort(list);
 
-        sb.append(result.size()).append("\n");
+        sb.append(count).append("\n");
 
-        for (int num : result) {
-            sb.append(num).append("\n");
+        for(int i=0;i<list.size();i++){
+            sb.append(list.get(i)).append("\n");
         }
 
         System.out.println(sb);
 
 
+
     }
 
     static void dfs(int x, int y) {
-        visit[x][y] = true;
+        result++;
+        visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        for(int i=0;i<4;i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
 
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-                continue;
-            else {
-                if (map[nx][ny] == 1 && !visit[nx][ny]) {
-                    count++;
-                    dfs(nx, ny);
+            if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
+                if(map[nextX][nextY] == 1 && !visited[nextX][nextY]) {
+                    dfs(nextX, nextY);
                 }
             }
-
         }
     }
 
 
-    public static void main(String args[]) throws Exception {
-        Main.solution();
+    public static void main(String[] args) throws IOException {
+        solution();
     }
 }
-
