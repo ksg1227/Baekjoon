24e73059c185b2262385c331851bc87315faf9bb
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2025-01-21 19:33:14 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,78 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/*
-    단계별로 풀어보기
-    동적 계획법 1
-    1149번 문제: RGB 거리
-*/
-
+/**
+ * 스터디 4주차
+ * DP
+ * 백준 2579번 : 계단 오르기
+ */
 
 public class Main {
 
-    //해당 계단으로 도달할 수 있는 경우는 2가지.
-    //두 계단 아래에서 올라오거나, 한 계단 아래에서 올라오거나
-    //1차원 배열 : 몇 번째 계단인지
-    //2차원 배열 : 0, 1
-    //0 : 바로 아래 계단에서 올라온 경우
-    //1 : 두 계단 아래에서 올라온 경우
+    // 0 : 한 계단 아래에서 올라온 경우
+    // 1 : 두 계단 아래에서 올라온 경우
+    // 만약 둘 다 1이면 그 다음 위치로는 갈 수 없음
 
     static int[] stairs;
+    static int N;
     static int[][] dp;
 
-    static int n;
-
     static void solution() throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        stairs = new int[n];
-        dp = new int[n][2];
+        stairs = new int[N + 1];
+        dp = new int[N + 1][2];
 
-
-        for (int i = 0; i < n; i++) {
+        stairs[0] = 0;
+        for (int i = 1; i <= N; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
+        }
 
-            if (i == 0) {
-                dp[i][0] = stairs[i];
-                dp[i][1] = stairs[i];
-            }
+        dp[0][0] = stairs[0];
+        dp[0][1] = stairs[0];
 
-            if (i == 1) {
-                dp[i][0] = dp[i - 1][0] + stairs[i];
-                dp[i][1] = stairs[i];
+        dp[1][0] = stairs[1];
+        dp[1][1] = stairs[1];
+
+        // 한 계단 아래를 밟고 올라온 경우 : 0 -> 두 계단 아래는 밟지 않은 상태여야함
+        // 한 계단 아래를 밟지 않고 올라온 경우 : 1 -> 두 계단 아래를 밟았어도 상관 없음
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] += dp[i - 1][1] + stairs[i];    // dp[i][0] += dp[i-1][0] 은 불가함.
+
+            if (i - 2 >= 0) {
+                dp[i][1] += Math.max(dp[i - 2][0], dp[i - 2][1]) + stairs[i];
             }
         }
 
-        // 첫번째 계단에 오르는 경우의 수
-        // 두번째 계단에 오르는 경우의 수
-        // 미리 채워두기
-
-
-        for (int i = 2; i < n; i++) {
-            //바로 아래 계단에서 올라오는 경우
-            //바로 아래 계단은 그 계단보다 2계단 아래에서 올라온 경우여야만 함
-            dp[i][0] = dp[i-1][1] + stairs[i];
-
-            //2계단 아래에서 올라오는 경우
-            //2계단 아래 계단은 그 계단보다 1계단 아래이던, 2계단 아래에서 올라오던 상관 없음
-            dp[i][1] = Math.max(dp[i-2][0], dp[i-2][1]) + stairs[i];
-        }
-
-        System.out.println(Math.max(dp[n-1][0], dp[n-1][1]));
+        System.out.println(Math.max(dp[N][0], dp[N][1]));
 
 
     }
 
-
-    public static void main(String args[]) throws Exception {
-        Main.solution();
+    public static void main(String[] args) throws IOException {
+        solution();
     }
+
 }
+
+
+
+
+
 
 
