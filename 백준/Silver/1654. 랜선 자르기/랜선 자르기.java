7a9687bbbc9a299b759fb3b43533b43a5073876c
ksg1227 <66379250+ksg1227@users.noreply.github.com>
2025-02-02 01:06:48 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,69 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
-/*
-    solved.ac
-    class2
-    1654번 문제 : 랜선 자르기
+/**
+ * 스터디 5주차
+ * 이진 탐색
+ * 백준 1654번 : 랜선 자르기
  */
 
 public class Main {
 
+    // K개의 랜선을 가지고 특정 길이로 잘랐을 때, N개 이상의 랜선이 만들어지면 됨.
+    // 단순히 완전 탐색을 할거면, 주어진 K개의 랜선 중 가장 큰 값부터 1까지 차례대로 줄여나가면서 잘라보면 됨
+    // 다만 최악의 경우 K = 10,000 이고 N 이 1,000,000 이라면 1,000,000 * 10,000 번의 연산이 필요해진다.
+    // 당연히 시간 초과. 그럼 어떻게? 이진 탐색
+    // 근데 이진 탐색 힌트 없이 생각 없이 들어가면, 뒤에서부터 찾을 수도 있을 것 같음. 그냥 테스트겸 해보자
+
+    static int K;
+    static int N;
+
+    static int[] lan;
+    static long result = Integer.MIN_VALUE;
+
     static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        //N개보다 많이 만드는 것도 N개를 만드는 것에 포함된다??
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        ArrayList<Long> list = new ArrayList<>();
-
-        long max = Integer.MIN_VALUE;
+        lan = new int[K];
 
         for (int i = 0; i < K; i++) {
-            long num = Long.parseLong(br.readLine());
-            list.add(num);
-
-            max = Math.max(max, num);
+            lan[i] = Integer.parseInt(br.readLine());
         }
 
-        long count;
+        Arrays.sort(lan);
 
-        long right = max;
-        long left = 1;
+        int low = 1;
+        int high = lan[lan.length - 1];
 
-        if(N == 1){
-            System.out.println(max);
-        }else {
-            while(left <= right){
-                count = 0;
-                long mid = (left + right) / 2;
+        binarySearch(low, high);
 
-                for(long num : list){
-                    count += (num / mid);
-                }
+        System.out.println(result);
 
-                if(count >= N){
-                    left = mid + 1;
-                }else{
-                    right = mid - 1;
-                }
+    }
+
+    static void binarySearch(long low, long high) {
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+
+            if (countLan(mid) >= N) {
+                low = mid + 1;
+                result = Math.max(mid, result);
+            } else {
+                high = mid - 1;
             }
+        }
+    }
 
-            System.out.println(right);
+    static int countLan(long length) {
+        int count = 0;
+        for (int i = 0; i < K; i++) {
+            count += lan[i] / length;
         }
 
+        return count;
     }
 
-    public static void main(String[] args) throws Exception {
-        Main.solution();
+    public static void main(String[] args) throws IOException {
+        solution();
     }
+
 }
+
+
+
+
+
+
+
