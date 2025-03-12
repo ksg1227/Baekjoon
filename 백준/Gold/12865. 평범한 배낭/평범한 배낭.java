8c0ee1f5c7bf98a693468a7782531cd12d5b9ab0
김상균 <66379250+ksg1227@users.noreply.github.com>
2025-03-12 13:50:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 55232 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -33,31 +33,20 @@ public class Main {
             int W = Integer.parseInt(st.nextToken());
             int V = Integer.parseInt(st.nextToken());
 
-            // 맨 처음을 채우고 시작 -> 물건은 하나임
-            if (i == 1) {
-                for (int j = 0; j <= K; j++) {
-                    if(j >= W) {
-                        dp[i][j] = V;
-                    }
-                }
-            } else {
 
-                // 가방 용량이 K일 때 최고 가치 갱신
-                for (int j = 1; j <= K; j++) {
-                    // 못 넣는 경우 = 가방의 용량이 현재 물건의 무게보다 적은 경우
-                    if (j < W) {
-                        dp[i][j] = dp[i - 1][j];
-                        continue;
-                    } else { // 넣을 수 있는 경우 -> 넣는 게 이득인지, 안 넣는게 이득인지 판단
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
-                    }
+            // 가방 용량이 K일 때 최고 가치 갱신
+            for (int j = 1; j <= K; j++) {
+                // 못 넣는 경우 = 가방의 용량이 현재 물건의 무게보다 적은 경우
+                if (j < W) {
+                    dp[i][j] = dp[i - 1][j];
+                } else { // 넣을 수 있는 경우 -> 넣는 게 이득인지, 안 넣는게 이득인지 판단
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
                 }
             }
         }
 
+
         System.out.println(dp[N][K]);
-
-
     }
 
 
