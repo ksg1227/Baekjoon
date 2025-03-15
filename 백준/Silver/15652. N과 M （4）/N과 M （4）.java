d3100352cc6d61cdb54f14495ee43f4c09c2dd60
김상균 <66379250+ksg1227@users.noreply.github.com>
2025-03-15 23:58:15 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17216 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,62 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
-/*
-    단계별로 풀어보기
-    백트래킹
-    15650번 문제: N과 M(2)
-*/
+/**
+ * 스터디 10주차
+ * DP
+ * 백준 15652번 : N과 M(4)
+ */
 
 public class Main {
 
-    //값을 담을 배열
-    static int[] arr;
+    static int N;
+    static int M;
+
+    static int[] nums;
 
     static StringBuilder sb = new StringBuilder();
 
     static void solution() throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
+        nums = new int[M];
 
-        dfs(N, M, 0, 1);
+        backTracking(0, 0, 1);
 
         System.out.println(sb);
 
+
     }
 
-    static void dfs(int N, int M, int depth, int at) {
-        if (depth == M) {
-            for (int val : arr) {
-                sb.append(val).append(" ");
+    static void backTracking(int count, int index, int start) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(nums[i]).append(" ");
             }
             sb.append("\n");
-
             return;
         }
 
-        for (int i = at; i <= N; i++) {
-            arr[depth] = i;
-            dfs(N, M, depth + 1, i);
+        for (int i = start; i <= N; i++) {
+            nums[index] = i;
+            backTracking(count + 1, index + 1, i);
+            nums[index] = 0;
         }
     }
 
 
-    public static void main(String[] args) throws Exception {
-        Main.solution();
+    public static void main(String[] args) throws IOException {
+        solution();
     }
+
 }
+
+
+
+
+
 
 
 
