8374e97654c93fe9a46b80110ddd4010ad4f6122
김상균 <66379250+ksg1227@users.noreply.github.com>
2025-06-02 00:02:01 +0900

    [level 2] Title: 전화번호 목록, Time: 306.73 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,7 +7,10 @@ class Solution {
         Arrays.sort(phone_book);
         
         for(int i=0;i<phone_book.length-1;i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) answer =false;
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
         }
         
         return answer;
