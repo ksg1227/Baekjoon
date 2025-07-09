fe78da801b9217cf7d6be2286047b8b0961ba549
김상균 <66379250+ksg1227@users.noreply.github.com>
2025-07-09 18:50:26 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,25 +2,16 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        // 10, 2
-        
-        // [4, 3] => 4 * 2 + 3 * 2 - 4 = 14 - 4 = 10
-        
-        // X * 2 + Y * 2 = brown + 4
-        
-        //
-        
-        for(int i=1;i<=5000;i++) {
-            for(int j=1;j<=i;j++) {
-                if(i * 2 + j * 2 == brown + 4) {
-                    if(i * j - brown == yellow) {
-                        answer[0] = i;
-                        answer[1] = j;
-                        break;
-                    }
-                }
+        for(int i=1;i<=2049;i++) {
+            int width = ((brown + 4) - (i * 2)) / 2;
+            
+            if((i * 2 + width * 2 == brown + 4) && (i*width - brown == yellow)) {
+                answer[0] = width;
+                answer[1] = i;
+                break;
             }
         }
+        
         return answer;
     }
 }
