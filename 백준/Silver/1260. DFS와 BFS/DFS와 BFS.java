b2baa50c807b79a2b1eefeb3aa4611301b245402
김상균 <66379250+ksg1227@users.noreply.github.com>
2026-04-08 19:10:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 20272 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,114 +1,95 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-
-/*
-    solved.ac
-    class3
-    1260번 문제 : DFS와 BFS
- */
-
 public class Main {
+	
+	static int N, M, V;
+	
+	static PriorityQueue<Integer>[] dfsAdjList, bfsAdjList;
+	static boolean[] visited;
+	
+	static StringBuilder forDfs, forBfs;
 
-    static int[][] graph;
-    static int N;
-    static int M;
-    static int V;
-    static boolean[] visit;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		
+		dfsAdjList = new PriorityQueue[N+1];
+		bfsAdjList = new PriorityQueue[N+1];
+		
+		visited = new boolean[N+1];
+		
+		forDfs = new StringBuilder();
+		forBfs = new StringBuilder();
+		
+		for(int i=1;i<=N;i++) {
+			dfsAdjList[i] = new PriorityQueue<>();
+			bfsAdjList[i] = new PriorityQueue<>();
+		}
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			dfsAdjList[start].add(end);
+			dfsAdjList[end].add(start);
+			
+			bfsAdjList[start].add(end);
+			bfsAdjList[end].add(start);
+		}
 
-    static Queue<Integer> queue = new LinkedList<>();
+		visited[V] = true;
+		dfs(V);
+		Arrays.fill(visited, false);
+		bfs(V);
+		
+		System.out.println(forDfs);
+		System.out.println(forBfs);
+		
+	}
+	
+	static void dfs(int current) {
+		
+		forDfs.append(current).append(" ");
+		
+		while(!dfsAdjList[current].isEmpty()) {
+			int next = dfsAdjList[current].poll();
+			
+			if(!visited[next]) {
+				visited[next] = true;
+				dfs(next);
+			}
+		}
+	}
+	
+	static void bfs(int start) {
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(start);
+		visited[start] = true;
+		
+		while(!queue.isEmpty()) {
+			int num = queue.poll();
+			
+			forBfs.append(num).append(" ");
+			
+			while(!bfsAdjList[num].isEmpty()) {
+				int next = bfsAdjList[num].poll();
+				
+				if(!visited[next]) {
+					visited[next] = true;
+					queue.add(next);
+				}
+			}
+		}
+	}
 
-    static StringBuilder sb1 = new StringBuilder();
-    static StringBuilder sb2 = new StringBuilder();
-
-    static void solution() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-
-        graph = new int[N + 1][N + 1];
-        visit = new boolean[N + 1];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int vertex1 = Integer.parseInt(st.nextToken());
-            int vertex2 = Integer.parseInt(st.nextToken());
-
-            graph[vertex1][vertex2] = 1;
-            graph[vertex2][vertex1] = 1;
-        }
-
-        dfs(V);
-        clear();
-        repeat_Bfs(V);
-
-
-        System.out.println(sb1);
-        System.out.println(sb2);
-
-    }
-
-    static void dfs(int vertexNum) {
-
-        sb1.append(vertexNum).append(" ");
-        visit[vertexNum] = true;
-
-        for (int i = 1; i <= N; i++) {
-            if (graph[vertexNum][i] == 1 && visit[i] == false) {
-                dfs(i);
-            }
-        }
-    }
-
-//    static void recursive_Bfs(int vertexNum) {
-//
-//        visit[vertexNum] = true;
-//        sb2.append(vertexNum).append(" ");
-//
-//        for (int i = 1; i <= N; i++) {
-//            if (graph[vertexNum][i] == 1 && visit[i] == false
-//                    && !queue.contains(i)) {
-//                queue.add(i);
-//            }
-//        }
-//
-//        while (!queue.isEmpty()) {
-//            bfs(queue.poll());
-//        }
-//    }
-
-    static void repeat_Bfs(int vertexNum) {
-
-        visit[vertexNum] = true;   //첫번째 정점을 위한 부분
-        queue.add(vertexNum);      //마찬가지로 첫번째 정점을 위한 부분
-
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll();
-            sb2.append(vertex).append(" ");
-            for (int i = 1; i <= N; i++) {
-                if (graph[vertex][i] == 1 && visit[i] == false && !queue.contains(i)) {
-                    visit[i] = true;
-                    queue.add(i);
-                }
-            }
-        }
-    }
-
-
-    static void clear() {
-        for (int i = 0; i < visit.length; i++) {
-            visit[i] = false;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Main.solution();
-    }
 }
